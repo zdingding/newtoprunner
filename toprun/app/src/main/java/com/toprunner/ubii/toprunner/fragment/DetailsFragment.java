53619ac0ed4791d573dbc1c6d0d02920cc98a6f3
zdingding <zd_jobing@163.com>
2016-09-30 17:08:58 +0800

    添加日历控件

--- toprun/app/src/main/java/com/toprunner/ubii/toprunner/fragment/DetailsFragment.java
+++ toprun/app/src/main/java/com/toprunner/ubii/toprunner/fragment/DetailsFragment.java
@@ -3,14 +3,19 @@ package com.toprunner.ubii.toprunner.fragment;
 import android.os.Bundle;
 import android.view.View;
 
+import com.test.sign_calender.DatePicker2;
 import com.toprunner.ubii.toprunner.R;
 import com.toprunner.ubii.toprunner.base.BaseFragment;
+
+import java.util.Calendar;
 
 /**
  * Created by ${赵鼎} on 2016/9/27 0027.
  */
 
 public class DetailsFragment extends BaseFragment {
+
+    private DatePicker2 picker;
 
     @Override
     public void setListener() {
@@ -19,7 +24,13 @@ public class DetailsFragment extends BaseFragment {
 
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
-
+        picker = (DatePicker2) findViewById(R.id.main_dp);
+        picker.setFestivalDisplay(true); //是否显示节日
+        picker.setHolidayDisplay(true); //是否显示假期
+        picker.setDeferredDisplay(true); //是否显示补休
+        Calendar c = Calendar.getInstance();//首先要获取日历对象
+        int mYear = c.get(Calendar.YEAR); // 获取当前年份
+        final int mMonth = c.get(Calendar.MONTH) + 1;// 获取当前月份
     }
 
     @Override
